6365ae0a15cf120ad53415b980868fb6d54ecad7
NHD1385 <88185720+NHD1385@users.noreply.github.com>
2023-03-08 22:50:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,16 +3,16 @@ const CONFIG = {
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     vào tim anh.`,
-    btnIntro: 'hihi',
+    btnIntro: 'hehe',
     title: '8/3 vui vẻ.',
-    desc: 'Chúc em ngày càng xin đẹp,trẻ trung và luôn giữ nụ cười trên môi(Vì em đẹp nhất khi e cười :>) ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Làm vợ anh nha!!.',
-    btnAccept: 'Dạ,em đồng ý! <3',
-    messLink: 'https://www.facebook.com/profile.php?id=100014982101534' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    desc: 'Chúc em ngày càng xinh đẹp,trẻ trung và luôn giữ nụ cười trên môi(Vì em đẹp nhất khi e cười 😉) ',
+    btnYes: 'Tớ cũng iuuu cậu nhìu💖💖💖',
+    btnNo: 'Không, tớ nghĩ chúng ta là bạn vẫn hơn💛💛💛',
+    question:'Trên thế giới hơn 7 tỉ người mà sao cậu lại iuu tớ <3',
+    btnReply: 'Gửi tớ nha!!!',
+    reply: 'Này cậu, tớ chỉ muốn cho cậu biết rằng tớ đang nghĩ về cậu rất rất nhìuuuuuuuu!',
+    mess: 'Tớ biết mà 🥰. Iuuuu cậu nhiều nhiều 😘😘',
+    messDesc: 'Làm ngừi iuuuu tớ nha!!.',
+    btnAccept: 'Oki, tớ đồng ý ',
+    messLink: 'https://www.facebook.com/cuncon.nguyen.359' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
